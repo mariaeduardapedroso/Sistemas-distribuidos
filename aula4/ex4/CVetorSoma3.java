5d169584520433997ad7bad7ddf8bb112587b454
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2024-10-14 18:05:05 -0300

    Create CVetorSoma3.java

---
+++ aula4/ex4/CVetorSoma3.java
@@ -0,0 +1,20 @@
+class CVetorSoma3 {
+	private int sum;
+	int somarVetor(int nums[]) {
+		sum = 0; 
+
+		for(int i=0; i<nums.length; i++) { 
+			sum += nums[i]; 
+			System.out.println("Total da " + Thread.currentThread().getName() + " eh " + sum);
+			
+			try { 
+				Thread.sleep(10);
+			
+			
+			}
+			catch(InterruptedException exc) {
+				System.out.println("Thread interrompida.");
+			} 
+		} 
+		return sum;
+	}}
